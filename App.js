50e06beb552a0dd555083defa0a32e5c5be01f36
superdev999 <superdev999@outlook.com>
2019-05-13 16:20:28 +0200

    init

---
+++ App.js
@@ -0,0 +1,23 @@
+import {createStackNavigator, createAppContainer} from 'react-navigation';
+import Home from './Components/Home'
+import Login from './Components/Login'
+import SignUp from './Components/SignUp'
+import DashBoard from './Components/DashBoard'
+import LiveValution from './Components/LiveValution'
+import ForgotPassword from './Components/ForgotPassword'
+import React, { Component } from 'react';
+import { AppRegistry, View } from 'react-native';
+const MainNavigator = createStackNavigator({
+  
+  Home: {screen: Home},
+  LiveValution:{screen:LiveValution},
+  LiveValution:{screen:LiveValution},
+  Login:{screen:Login},
+  SignUp:{screen:SignUp},
+  DashBoard:{screen:DashBoard},
+  ForgotPassword:{screen:ForgotPassword}
+});
+
+const App = createAppContainer(MainNavigator);
+
+export default App;
